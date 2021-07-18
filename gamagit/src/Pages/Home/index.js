7043abe2f026d6de57a5d1e504a1a7a15c62cb50
William Kiyoshi Taniguchi <wilkiyoshi>
2021-07-18 18:39:53 -0300

    atualização

---
+++ gamagit/src/Pages/Home/index.js
@@ -0,0 +1,37 @@
+
+import React, {useState} from 'react';
+import axios from 'axios';
+import * as S from './styled';
+import { useHistory } from 'react-router-dom'
+
+function App(props) {
+  const history = useHistory();
+  const [ usuario, setUsuario ] = useState('');
+  function handlePesquisa(){
+      console.log({usuario});
+      axios.get(`https://api.github.com/users/${usuario}/repos`).then(response => {
+        const repositories = response.data;
+        const repositoriesName = [];
+        repositories.map((respository) => {
+          repositoriesName.push(respository.name);
+        });
+        //console.log(repositoriesName);
+        localStorage.setItem('repositoriesName', JSON.stringify(repositoriesName));
+        history.push ('/repositories')
+        
+      });
+
+  }
+  return (
+    <S.Container>
+    <h1>{props.title}{props.user}</h1>
+    <S.Input placeholder="Usuário" className="usuarioInput" value={usuario} onChange={e => setUsuario(e.target.value)}></S.Input>
+    <S.Button type='button' onClick={handlePesquisa}>Pesquisar</S.Button>
+    </S.Container>
+  );
+
+}
+
+export default App;
+
+//JSX código HTML dentro de um script
---
+++ gamagit/src/Pages/Repositories/index.js
@@ -0,0 +1,32 @@
+import React, { useEffect , useState } from 'react';
+import { Link } from 'react-router-dom';
+import * as S from './styled';
+
+export default function Repositories(){
+    
+    const [ repositories, setRepositories ] = useState([]);
+    useEffect(() => {
+        let repositoriesName = localStorage.getItem('repositoriesName');
+        repositoriesName = JSON.parse(repositoriesName);
+        //console.log(repositoriesName);
+        setRepositories(repositoriesName);
+        //localStorage.clear();
+    },[]);
+
+    return (
+        <>
+        <S.Container>
+        <S.Title>Repositórios</S.Title>
+        <S.List>
+            {repositories.map(repository=> {
+                return(
+                    <S.ListItem>Repositório: {repository}</S.ListItem>
+                )})}
+        </S.List>  
+        <S.LinkHome to='/'> Voltar </S.LinkHome>
+        </S.Container>
+        </>
+    )
+};
+
+//export default Repositories;
