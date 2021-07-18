7287c9ec063bfebb52bcf79ef52cd59f271c655b
William Kiyoshi Taniguchi <wilkiyoshi>
2021-07-18 16:25:17 -0300

    atualização

--- gamagit/src/index.js
+++ gamagit/src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+
 ReactDOM.render(
   //<React.StrictMode>
     <App title="Hello" user="Gama Academy"/>,
   //</React.StrictMode>,
   document.getElementById('root')
+
 );
 
 // If you want to start measuring performance in your app, pass a function
